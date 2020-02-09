425ce31ba55fe0bc727e2c4070dc1a7ef12dd2e9
Amy de Buitléir <amy@nualeargais.ie>
2020-02-09 15:52:21 +0000

    updated year

--- fix-copyright.sh
+++ fix-copyright.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
-#: Update the copyright date for all Haskell files in the current Stack project.
+#: Update the copyright date for all Haskell files in and under the current directory.
 #: You will see a preview of all changes before they are made.
 
 for file in `find . -regextype posix-egrep -regex '.*\.hs|.*\.cabal|package.yaml|LICENSE' | grep -v '^./.stack-work' | 
 grep -v -e '/Setup.hs'`
 do
 	echo Editing $file
-	sed 's/(c) Amy de Buitléir \(20..\)$/(c) Amy de Buitléir \1-2019/; s/ \(20..\) Amy de Buitléir/ \1-2019 Amy de Buitléir/; s/-20../-2019/' $file > $file.backup
+	sed 's/(c) Amy de Buitléir \(20..\)$/(c) Amy de Buitléir \1-2020/; s/ \(20..\) Amy de Buitléir/ \1-2020 Amy de Buitléir/; s/-20../-2019/' $file > $file.backup
 	diff $file $file.backup
 	result=$?
 	if [ $result -gt 0 ] ; then 
