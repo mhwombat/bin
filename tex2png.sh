d0a9a7724073b17c2cd5529fb777e3fc4945b4ff
Amy de Buitléir <amy@nualeargais.ie>
2020-02-18 16:18:34 +0000

    updated docs

--- tex2png.sh
+++ tex2png.sh
@@ -3,19 +3,23 @@
 #: You might use this, for example, if you want to display a short LaTeX document as an image within an HTML page.
 #:
 #: Usage example:
-#:
+#: 
+#: ```
 #:    tex2png.sh sample.png < sample.tex
-#:
+#: ```
+#: 
 #: You will probably want to use the varwidth parameter on documentclass,
 #: and override the default margins.
 #: Here's a sample minimal LaTeX document that you can use as a starting point.
-#:
+#: 
+#: ```
 #:   \documentclass[varwidth,convert,margin=4pt]{standalone}
 #:   \begin{document}
 #:   \huge
 #:   Hello world!
 #:   \end{document}
-#:
+#: ```
+#: 
 OUTFILE=$(readlink -f $1)
 TMPDIR=$(mktemp -d "tex2png_XXXXX")
 
