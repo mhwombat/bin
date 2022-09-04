00881ded92239d125e8b79bbd2d140451b8b2237
Amy de Buitléir <amy@nualeargais.ie>
2022-09-04 19:31:36 +0100

    update year automatically

--- fix-copyright.sh
+++ fix-copyright.sh
@@ -2,28 +2,29 @@
 #: Update the copyright date for all Haskell files in and under the current directory.
 #: You will see a preview of all changes before they are made.
 
+YEAR=`date +%Y`
+
 for file in `find . \( -name '*.hs' -o -name '*.cabal' -o -name LICENSE \)`
 
 do
 	echo Editing $file
-#	sed 's/(c) Amy de Buitléir \(20..\)$/(c) Amy de Buitléir \1-2022/; s/ \(20..\) Amy de Buitléir/ \1-2020 Amy de Buitléir/; s/-20../-2022/' $file > $file.backup
-        sed '
+        sed "
              s/(c) Amy de Buitléir \(.*\)/(c) \1 Amy de Buitléir/
-             s/\((c) 20..\)$/\1-2022/
-             s/\((c) 20..\)-..../\1-2022/
-             s/\(copyright: *20..\)$/\1-2022/
-             s/\(copyright: *20..\)-..../\1-2022/
-            ' $file > $file.backup
+             s/\((c) 20..\)$/\1-${YEAR}/
+             s/\((c) 20..\)-..../\1-${YEAR}/
+             s/\(copyright: *20..\)$/\1-${YEAR}/
+             s/\(copyright: *20..\)-..../\1-${YEAR}/
+            " $file > $file.backup
 	diff $file $file.backup
 	result=$?
-	if [ $result -gt 0 ] ; then 
+	if [ $result -gt 0 ] ; then
 		if [ ! -z "$force" ]; then
 		    answer="y"
 		else
 		    read -p "ok (y/n) " answer
 		    echo "answer=" $answer
 		fi
-		if [ "$answer" = "y" ] ; then 
+		if [ "$answer" = "y" ] ; then
 			cp $file.backup $file
 			echo File replaced
 		fi
